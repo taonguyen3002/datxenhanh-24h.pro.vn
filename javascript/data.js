305b2f9dedcb4d2f8a2121961038e6136019c4e9
taonguyen3002 <taonhung298@gmail.com>
2024-12-21 19:53:01 +0700

    update

--- javascript/data.js
+++ javascript/data.js
@@ -6,6 +6,55 @@ let posts = [
     link: "https://datxenhanh-24h.pro.vn",
     img: "../assets/image/grab.jpg",
   },
+  {
+    id: 2,
+    title:
+      "Đặt Xe Grab Đồng Nai - Tổng Đài Đặt Xe Ôm Taxi Giao Hàng 0559 023 567",
+    link: "https://datxenhanh-24h.pro.vn/blogs/grab-dong-nai",
+    img: "../assets/image/grab.jpg",
+  },
+  {
+    id: 3,
+    title:
+      "Đặt Xe Grab Biên Hòa Đồng Nai - Tổng Đài Đặt Xe Ôm Taxi Giao Hàng Biên Hòa 0559 023 567",
+    link: "https://datxenhanh-24h.pro.vn/blogs/grab-dong-nai/grab-bien-hoa",
+    img: "../assets/image/grab.jpg",
+  },
+  {
+    id: 4,
+    title:
+      "Đặt Xe Grab Vĩnh Cửu Đồng Nai - Tổng Đài Đặt Xe Ôm Taxi Giao Hàng Vĩnh Cửu 0559 023 567",
+    link: "https://datxenhanh-24h.pro.vn/blogs/grab-dong-nai/grab-vinh-cuu",
+    img: "../assets/image/grab.jpg",
+  },
+  {
+    id: 5,
+    title:
+      "Đặt Xe Grab Long Thành Đồng Nai - Tổng Đài Đặt Xe Ôm Taxi Giao Hàng Long Thành 0559 023 567",
+    link: "https://datxenhanh-24h.pro.vn/blogs/grab-dong-nai/grab-long-thanh",
+    img: "../assets/image/grab.jpg",
+  },
+  {
+    id: 6,
+    title:
+      "Đặt Xe Grab Nhơn Trạch Đồng Nai - Tổng Đài Đặt Xe Ôm Taxi Giao Hàng Nhơn Trạch 0559 023 567",
+    link: "https://datxenhanh-24h.pro.vn/blogs/grab-dong-nai/grab-nhon-trach",
+    img: "../assets/image/grab.jpg",
+  },
+  {
+    id: 7,
+    title:
+      "Đặt Xe Grab Trảng Bom Đồng Nai - Tổng Đài Đặt Xe Ôm Taxi Giao Hàng Trảng Bom 0559 023 567",
+    link: "https://datxenhanh-24h.pro.vn/blogs/grab-dong-nai/grab-trang-bom",
+    img: "../assets/image/grab.jpg",
+  },
+  {
+    id: 7,
+    title:
+      "Đặt Xe Grab Long Khánh Đồng Nai - Tổng Đài Đặt Xe Ôm Taxi Giao Hàng Long Khánh 0559 023 567",
+    link: "https://datxenhanh-24h.pro.vn/blogs/grab-dong-nai/grab-long-khanh",
+    img: "../assets/image/grab.jpg",
+  },
 ];
 
 function fetchPosts() {
